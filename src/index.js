73f09b54d6688d3dd95cf3577a87b01d6abc24ee
italiya07 <drashtiitaliya07@gmail.com>
2024-08-07 01:36:48 -0500

    Feat: intialize postgres database and remove middleware file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import sequelize from "./config/database.js";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
